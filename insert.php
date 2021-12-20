d23a2caaa60ce84c39355970f6a8eafbc0f5af1d
UrosJovanovic9 <urosh.jovanovicc@gmail.com>
2021-12-20 10:01:43 +0100

    Uradjena registracija novih korisnika

--- insert.php
+++ insert.php
@@ -5,7 +5,8 @@ $username = "root";
 $password = "";
 $dbname = "bazaphp";
 
-$conn = mysqli_connect($server,$username,$password,$dbname);
+
+$connection = mysqli_connect($server,$username,$password,$dbname);
 
 if(isset($_POST['submit'])){
     if(!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])){
@@ -15,10 +16,12 @@ if(isset($_POST['submit'])){
 
         $query = "insert into korisnik(ime,mejl,lozinka) values('$username','$email','$password')";
 
-        $run = mysqli_query($conn,$query) or die(mysqli_error());
+        $run = mysqli_query($connection,$query) or die(mysqli_error());
 
         if($run){
             echo "Uspesno ste se prijavili!";
+            header("Location: index.php");
+            exit();
         }else{
             echo "Podaci nisu potvrdjeni";
         }
@@ -28,4 +31,6 @@ if(isset($_POST['submit'])){
     }
 }
 
+// $ime = "Pera";
+// echo "Pozdrav $ime!"
 ?>
